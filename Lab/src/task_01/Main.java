3f7f2f052568169a595feaf9ec8c0415e2ea9e98
JENS\Jens <je.harder@gmail.com>
2017-10-28 11:55:51 +0200

    #1 Aufgabe 1 gelöst

--- Lab/src/task_01/Main.java
+++ Lab/src/task_01/Main.java
@@ -1,7 +1,32 @@
 package task_01;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("&Test");
+
+		Scanner scanner = new Scanner(System.in);
+
+		if (scanner.hasNextInt()) {
+
+			int a;
+			int b;
+
+			for (int coutner = scanner.nextInt(); coutner > 0; coutner--) {
+
+				a = scanner.nextInt();
+				b = scanner.nextInt();
+
+				if (a < b) {
+					System.out.println("less than");
+				} else if (a > b) {
+					System.out.println("greater than");
+				} else {
+					System.out.println("equal to");
+				}
+			}
+		}
+
+		scanner.close();
 	}
 }
