0a34255e2238788dbe455c3d404b24333f487ebd
P4nd4b43r <33057228+P4nd4b43r@users.noreply.github.com>
2017-10-29 13:11:17 +0100

    check for invalid input
    not checked on server

--- Lab/src/sheet01/task_03/Main.java
+++ Lab/src/sheet01/task_03/Main.java
@@ -12,53 +12,58 @@ public class Main {
 			String str;
 			int m;
 			int n;
-
-			for (int coutner = scanner.nextInt(); coutner > 0; coutner--) {
+			int coutner = scanner.nextInt();
+			// check for invalid input
+			if ( counter > 100) 
+				counter = 100;
+			for (counter; coutner > 0; coutner--) {
 
 				str = scanner.next();
 				m = scanner.nextInt();
 				n = scanner.nextInt();
+			// check for invalid input
+				if ( m > 100 || m < 4 || n > 100 || n < 4) 
+					System.out.println("invalid input");
+				else {
+					if (str.equals("K")) {// king
+						System.out.println((int) Math.ceil((m) / 2.0) * (int) Math.ceil((n) / 2.0));
+					} else if (str.equals("Q")) {// queen
+						int smaller = m < n ? m : n;
+						int bigger = m < n ? n : m;
 
-				if (str.equals("K")) {// king
-					System.out.println((int) Math.ceil((m) / 2.0) * (int) Math.ceil((n) / 2.0));
-				} else if (str.equals("Q")) {// queen
-					int smaller = m < n ? m : n;
-					int bigger = m < n ? n : m;
-
-					// simple cases, because assume smaller < 5
-					switch (smaller) {
-					case 1:
-						System.out.println(1);
-						break;
-					case 2:
-						if (bigger < 3)
+						// simple cases, because assume smaller < 5
+						switch (smaller) {
+						case 1:
 							System.out.println(1);
-						else
-							System.out.println(2);
-						break;
-					case 3:
-						if (bigger < 4)
-							System.out.println(2);
-						else
-							System.out.println(3);
-						break;
-					case 4:
-						if (bigger < 5)
-							System.out.println(3);
-						else
-							System.out.println(4);
-						break;
-					default:
-						System.out.println("error");
-						break;
+							break;
+						case 2:
+							if (bigger < 3)
+								System.out.println(1);
+							else
+								System.out.println(2);
+							break;
+						case 3:
+							if (bigger < 4)
+								System.out.println(2);
+							else
+								System.out.println(3);
+							break;
+						case 4:
+							if (bigger < 5)
+								System.out.println(3);
+							else
+								System.out.println(4);
+							break;
+						default:
+							System.out.println("error");
+							break;
+						}
+					} else if (str.equals("r")) {// rook
+						System.out.println(Math.min(m, n));
+					} else if (str.equals("k")) {// knight
+						System.out.println((int) Math.ceil((m * n) / 2.0));
 					}
-
-				} else if (str.equals("r")) {// rook
-					System.out.println(Math.min(m, n));
-				} else if (str.equals("k")) {// knight
-					System.out.println((int) Math.ceil((m * n) / 2.0));
 				}
-
 			}
 		}
 
