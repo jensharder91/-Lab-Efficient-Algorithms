b003d2dfc368b28a2205efe71d5a428299cf9186
P4nd4b43r <33057228+P4nd4b43r@users.noreply.github.com>
2017-10-29 12:55:06 +0100

    my suggestion
    not checked on the server

--- Lab/src/sheet02/task_01/Main.java
+++ Lab/src/sheet02/task_01/Main.java
@@ -1,5 +1,37 @@
 package sheet02.task_01;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		if (scanner.hasNextInt()) {
+
+			int n;
+			int k;
+      long sum;
+      
+			while (scanner.hasNextInt()) {
+        sum = 0;
+				n = scanner.nextInt();
+				k = scanner.nextInt();
+
+        //check for valid input
+        if ( (n > 0 && n < 146) && ( k >0 && k < 16)) {
+        
+				  for ( int j=1;  j > n; j++){
+           sum += j* (int)Math.pow(k, j);
+          }
+				  System.out.println(sum);
+        }
+				else
+         System.out.println("invalid input");
+			}
+		}
+
+		scanner.close();
+	}
 }
+
