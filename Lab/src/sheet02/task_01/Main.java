bdc8fdcb1fffc4ad7af3b5065af1cb1b94c2383f
P4nd4b43r <s6anhers@uni-bonn.de>
2017-11-14 20:20:29 +0100

    BigInteger statt long

--- Lab/src/sheet02/task_01/Main.java
+++ Lab/src/sheet02/task_01/Main.java
@@ -1,5 +1,6 @@
 package sheet02.task_01;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
@@ -11,27 +12,32 @@ public class Main {
 
 			int n;
 			int k;
-			long sum;
+			BigInteger zero = BigInteger.valueOf(0);
+			BigInteger sum; 
+			BigInteger temp;
       
 			while (scanner.hasNextInt()) {
-        sum = 0;
+				sum = zero;
 				n = scanner.nextInt();
 				k = scanner.nextInt();
+				temp= BigInteger.ONE;
 
-        //check for valid input
-        if ( (n > 0 && n < 146) && ( k >0 && k < 16)) {
+				//check for valid input
+				if ( (n > 0 && n < 146) && ( k >0 && k < 16)) {
         
 				  for ( int j=1;  j <= n; j++){
-           sum += j* (int)Math.pow(k, j);
-          }
-				  System.out.println(sum);
-        }
-				else
-         System.out.println("invalid input");
+					  temp=temp.multiply(BigInteger.valueOf(k));
+					  sum = sum.add(temp.multiply(BigInteger.valueOf(j)));
+				  }
+				  
+				  System.out.println(sum.toString());
+				}
+			
 			}
-		}
 
-		scanner.close();
+			scanner.close();
+		}
 	}
+		
 }
 
