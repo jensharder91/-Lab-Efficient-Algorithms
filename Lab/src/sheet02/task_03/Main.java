79d4eccd28a093b15d64a98de390c44ba9d5cf1b
P4nd4b43r <s6anhers@uni-bonn.de>
2017-11-16 19:49:16 +0100

    Sheet02_task03

--- Lab/src/sheet02/task_03/Main.java
+++ Lab/src/sheet02/task_03/Main.java
@@ -1,5 +1,55 @@
 package sheet02.task_03;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int player;
+		int [] index;
+		String attributes;
+		
+		while (scanner.hasNextInt()) {
+			player = scanner.nextInt();
+			if (player == 0)
+				break;
+			int Matrix [][][] =new int [26][26][26];
+			int number=0;
+			String [][] Spieler_Attr= new String [player][6];
+			attributes = scanner.nextLine();
+			for (int i=0; i< player; i++){
+				attributes = scanner.nextLine();
+				Spieler_Attr[i]= attributes.split(" ");
+				for (int j=0; j<6; j++){
+					index = AttrToIndex(Spieler_Attr[i][j]);
+					Matrix [index[0]][index[1]][index[2]]++;
+					if(Matrix [index[0]][index[1]][index[2]] > number)
+						number = Matrix [index[0]][index[1]][index[2]];
+				}
+			}
+			if (number == 1)
+				System.out.println(player);		
+			else
+				System.out.println(number);		
+		}
+		scanner.close();
+	}
+	public static int [] AttrToIndex(String s){
+		int [] indizes= new int[s.length()];
+		int [] value={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
+		char [] key = {'a','b','c','d','e','f','g','h','i','j','k','l',
+				'm','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		for (int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            for (int j = 0; j < key.length; j++) {
+                if (current == key[j]) {
+                    indizes[i]=value[j];
+                    break;
+                }
+            }
+        }
+		return indizes;
+	}
+	
 }
