8c15a7b2fe1cf0a1c34bf8f6750b9bf0bc15cdbf
JENS\Jens <je.harder@gmail.com>
2017-11-30 11:35:54 +0100

    sheet 3 task 4

--- Lab/src/sheet03/task_04_LongestIncreasingSubsequence/LongestIncreasingSubsequence.java
+++ Lab/src/sheet03/task_04_LongestIncreasingSubsequence/LongestIncreasingSubsequence.java
@@ -1,23 +1,49 @@
 package sheet03.task_04_LongestIncreasingSubsequence;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
+/*
+0
+8
+4
+12
+2
+10
+6
+14
+1
+9
+5
+13
+11
+7
+15
+ */
+//result 6
 public class LongestIncreasingSubsequence {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		Scanner scanner = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		ArrayList<Integer> nums = new ArrayList<>();
 
-		while (scanner.hasNext()) {
+		while (true) {
 			// String newInput = scanner.next();
 			// if(newInput.equals("") || newInput.equals("end")) {
 			// break;
 			// }
 			// int newNumber = Integer.valueOf(newInput);
+			
+			String input = br.readLine();
+			
+			if(input == null || input.equals("")) {
+				break;
+			}
 
-			int newNumber = scanner.nextInt();
+			int newNumber = Integer.valueOf(input);
 
 			if (nums.size() == 0 || newNumber > nums.get(nums.size() - 1)) {
 				// just add number to our list
@@ -41,7 +67,6 @@ public class LongestIncreasingSubsequence {
 		}
 
 		System.out.println(nums.size());
-		scanner.close();
 
 	}
 
