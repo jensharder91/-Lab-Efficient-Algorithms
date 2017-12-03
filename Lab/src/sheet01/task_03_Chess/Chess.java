d2d7c6e11f5aa2c646c0888bb38b4fa7d5307968
P4nd4b43r <s6anhers@uni-bonn.de>
2017-12-03 15:56:56 +0100

    fertig

--- Lab/src/sheet01/task_03_Chess/Chess.java
+++ Lab/src/sheet01/task_03_Chess/Chess.java
@@ -22,40 +22,7 @@ public class Chess {
 				if (str.equals("K")) {// king
 					System.out.println((int) Math.ceil((m) / 2.0) * (int) Math.ceil((n) / 2.0));
 				} else if (str.equals("Q")) {// queen
-					int smaller = m < n ? m : n;
-					int bigger = m < n ? n : m;
-
-					// simple cases, because assume smaller < 5
-					switch (smaller) {
-					case 0:
-						System.out.println(0);
-						break;
-					case 1:
-						System.out.println(1);
-						break;
-					case 2:
-						if (bigger < 3)
-							System.out.println(1);
-						else
-							System.out.println(2);
-						break;
-					case 3:
-						if (bigger < 4)
-							System.out.println(2);
-						else
-							System.out.println(3);
-						break;
-					case 4:
-						if (bigger < 4)
-							System.out.println(3);
-						else
-							System.out.println(4);
-						break;
-					default:
-						System.out.println("error");
-						break;
-					}
-
+					System.out.println(Math.min(m, n));
 				} else if (str.equals("r")) {// rook
 					System.out.println(Math.min(m, n));
 				} else if (str.equals("k")) {// knight
