132e1615c507517af25d0268f67a86b6d6746e90
JENS\Jens <je.harder@gmail.com>
2017-12-05 13:09:38 +0100

    sheet 3 task 3

--- Lab/src/sheet03/task_03_MaximumProduct/MaximumProduct.java
+++ Lab/src/sheet03/task_03_MaximumProduct/MaximumProduct.java
@@ -1,8 +1,9 @@
 package sheet03.task_03_MaximumProduct;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
-// 5     4 -5 2 -2 -30   3 -3 0 -4   3 0 0 0   3 -1 -2 -3   6 -20 2 -2 0 5 4 
+// 6     4 -5 2 -2 -30   3 -3 0 -4   3 0 0 0   3 -1 -2 -3   6 -20 2 -2 0 5 4    200   20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20 20
 // 120  ---  0  ---   0  --- 6  ---  80
 public class MaximumProduct {
 	public static void main(String[] args) {
@@ -16,31 +17,38 @@ public class MaximumProduct {
 
 			int input;
 
-			int maxPositive = 0;
-			int maxNegative = 0;
+			BigInteger maxPositive = BigInteger.ZERO;
+			BigInteger maxNegative = BigInteger.ZERO;
 
-			int max = 0;
+			BigInteger max = BigInteger.valueOf(0);
 
 			for (int j = 0; j < k; j++) {
 				input = scanner.nextInt();
 
 				if (input > 0) {
 					// positive number
-					maxPositive = Math.max(maxPositive * input, input);
-					maxNegative = Math.min(maxNegative * input, 0);
+					maxPositive = maxPositive.multiply(BigInteger.valueOf(input)).max(BigInteger.valueOf(input));
+					// maxPositive = Math.max(maxPositive * input, input);
+					maxNegative = maxNegative.multiply(BigInteger.valueOf(input)).min(BigInteger.ZERO);
+					// maxNegative = Math.min(maxNegative * input, 0);
 				} else if (input < 0) {
 					// negative number
-					int oldMaxPositive = maxPositive;
-					int oldMaxNegative = maxNegative;
-					maxPositive = Math.max(oldMaxNegative * input, 0);
-					maxNegative = Math.min(oldMaxPositive * input, input);
+					BigInteger oldMaxPositive = maxPositive;
+					BigInteger oldMaxNegative = maxNegative;
+					// int oldMaxPositive = maxPositive;
+					// int oldMaxNegative = maxNegative;
+					maxPositive = oldMaxNegative.multiply(BigInteger.valueOf(input)).max(BigInteger.ZERO);
+					// maxPositive = Math.max(oldMaxNegative * input, 0);
+					maxNegative = oldMaxPositive.multiply(BigInteger.valueOf(input)).min(BigInteger.valueOf(input));
+					// maxNegative = Math.min(oldMaxPositive * input, input);
 				} else if (input == 0) {
 					// reset maxpositive / maxNegative counter
-					maxPositive = 0;
-					maxNegative = 0;
+					maxPositive = BigInteger.ZERO;
+					maxNegative = BigInteger.ZERO;
 				}
 
-				max = Math.max(maxPositive, max);
+				// max = Math.max(maxPositive, max);
+				max = maxPositive.max(max);
 			}
 
 			System.out.println(max);
